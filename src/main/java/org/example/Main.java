5fe64888f3a234e6ec7bb01d5ee4380788ee4c4d
KovGr <KovGrDm@outlook.com>
2024-05-07 16:52:27 +0300

    hw61

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
 
     public static void main(String[] args) throws SQLException {
-        String connectionUrl =  "jdbc:h2:/Users/KovGr/h2/test";
+        String connectionUrl =  "jdbc:h2:./test";
         Connection connection = DriverManager.getConnection(connectionUrl);
     }
 
